4c73d2f8336a7e653b95cc88622802e4a46eb1c5
Mark Belotskiy <m.belotskiy@yuzhno-sakh.ru>
2016-03-24 17:31:40 +1000

    Added ContentProcessor class

---
+++ spec/ContentProcessorSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\ConstantNull\Backstubber;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ContentProcessorSpec extends ObjectBehavior
+{
+    function it_is_receives_and_returns_content()
+    {
+        $content = 'Live long and prosper';
+
+        $this->setContent($content);
+        $this->getContent()->shouldBeEqualTo($content);
+    }
+
+    function it_can_replace_specified_parts_of_content()
+    {
+        $content = 'This is the story of the StarshipName. Its mission: MissionDescription';
+
+        $this->setContent($content);
+
+        $this->replace('StarshipName', 'Starship Enterprise')
+             ->replace('MissionDescription', 'to explore the strange new worlds where no man has gone before')
+             ->process();
+
+        $this->getContent()->shouldBe(
+            'This is the story of the Starship Enterprise. Its mission: to explore the strange new worlds where no man has gone before'
+        );
+    }
+
+    function it_can_do_replaces_immediately()
+    {
+        $starshipName = 'Enterprise NCC 1701';
+
+        $this->setContent($starshipName);
+
+        $this->doReplace('1701', '1701 A');
+        $this->getContent()->shouldBeEqualTo('Enterprise NCC 1701 A');
+
+        $this->doReplace('1701 A', '1701 B');
+        $this->getContent()->shouldBeEqualTo('Enterprise NCC 1701 B');
+
+        $this->doReplace('1701 B', '1701 C');
+        $this->getContent()->shouldBeEqualTo('Enterprise NCC 1701 C');
+    }
+}
---
+++ src/ContentProcessor.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace ConstantNull\Backstubber;
+
+class ContentProcessor
+{
+    /**
+     * @var string processed content
+     */
+    protected $content;
+
+    /**
+     * array of searchable parts and their replacements
+     *
+     * @var array
+     */
+    protected $replacements = [];
+
+    /**
+     * Set content for processing
+     *
+     * @param $content string
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get current content
+     *
+     * @return string string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Immediately replaces part of content
+     *
+     * @param $searchFor
+     * @param $replaceWith
+     * @return $this
+     */
+    public function doReplace($searchFor, $replaceWith)
+    {
+        $this->content = str_replace($searchFor, $replaceWith, $this->content);
+
+        // for chaining
+
+        return $this;
+    }
+
+    /**
+     * Add replacement for processing
+     *
+     * @param $searchFor
+     * @param $replaceWith
+     * @return $this
+     */
+    public function replace($searchFor, $replaceWith)
+    {
+        $this->replacements[$searchFor] = $replaceWith;
+
+        // for chaining
+
+        return $this;
+    }
+
+    /**
+     * Do actual replacements
+     *
+     * @return $this
+     */
+    public function process()
+    {
+        // Actual replacements
+
+        $searchFor = array_keys($this->replacements);
+        $replaceWith = array_values($this->replacements);
+        $this->content = str_replace($searchFor, $replaceWith, $this->content);
+
+        // clearing replacements
+        $this->replacements = [];
+
+        // for chaining
+
+        return $this;
+    }
+}
