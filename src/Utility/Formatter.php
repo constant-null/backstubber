fb69dd94c1e2856d24257fe3add4afa42d06a5ab
Mark Belotskiy <constant.null@gmail.com>
2016-03-28 20:22:20 +1000

    Formatter::formatArray spec implemented

---
+++ src/Utility/Formatter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ConstantNull\Backstubber\Utility;
+
+class Formatter
+{
+    /**
+     * Return text representation of array
+     *
+     * @param array $array input array
+     * @param bool $braces add array braces to output
+     * @return string
+     */
+    public function formatArray(array $array, $braces = true)
+    {
+        $output = implode($array, "', '");
+
+        if ($output) {
+            $output = "'$output'";
+        }
+
+        if ($braces) {
+            $output = "[$output]";
+        }
+
+        return $output;
+    }
+}
