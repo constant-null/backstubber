0a055e90365137ae4b274ed601f650f553139529
Mark Belotskiy <constant.null@gmail.com>
2016-03-29 14:42:41 +1000

    FileGenerator spec and some test files added

---
+++ spec/FileGeneratorSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\ConstantNull\Backstubber;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use org\bovigo\vfs\vfsStream;
+
+class FileGeneratorSpec extends ObjectBehavior
+{
+    protected $vfs;
+
+    function let()
+    {
+        $this->vfs = vfsStream::setup('output');
+    }
+
+    function it_handles_basic_var_replaces()
+    {
+        $officers = ['James T Kirk', 'Mr. Spock', 'Scott Montgomery', 'Pavel Chekov', 'Nyota Uhura', 'hikaru sulu'];
+        $this->useStub('spec/Stubs/StarshipClass.blade.stub')
+             ->withDelimiters('{{', '}}')
+             ->set('officers', $officers)
+             ->set('captain', 'James T. Kirk')
+             ->set('crew', 430)
+             ->setRaw('class', 'Enterprise')
+             ->setRaw('namespace', 'Federation\\Ships')
+             ->generate('vfs://output/EnterpriseClass.php');
+
+        expect(file_get_contents('vfs://output/EnterpriseClass.php'))->toBeEqualTo(file_get_contents('spec/Stubs/StarshipClass.blade.assert'));
+    }
+}
